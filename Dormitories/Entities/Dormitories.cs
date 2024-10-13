60802e2a58f04c9515d80c2bb00796604fbdf313
Art Github <artGithubtest@hotmail.com>
2024-10-13 09:51:01 +0200

    completed dormitories adnd students classes

--- Dormitories/Entities/Dormitories.cs
+++ Dormitories/Entities/Dormitories.cs
@@ -3,5 +3,12 @@
     public class Dormitories
     {
         public int Id { get; set; }
+
+        public string Code {  get; set; }
+        public string Name { get; set; }
+        public int CurrentCapacity { get; set; }
+        public int MaxCapacity { get; set; }
+
+        public List<Students> StudentList = new List<Students>();
     }
 }
